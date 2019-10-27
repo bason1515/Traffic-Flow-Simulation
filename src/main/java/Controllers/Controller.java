ef0457337f07564d75637fef9e035395ca444fe2
bason1515 <bason1515@gmail.com>
2019-10-27 11:10:52 +0100

    Simulation Controller creates two different cars(car, truck)

--- src/main/java/Controllers/Controller.java
+++ src/main/java/Controllers/Controller.java
@@ -17,13 +17,20 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Controller {
-    private static final int CAR_NUMBER = 30;
+    private static final int CAR_NUMBER = 50;
     private static final int LINE_NUMBER = 3;
-    private static final double MIN_VELO = 20;
-    private static final double MAX_VELO = 50;
-    private static final double MIN_ACCE = 0.1;
-    private static final double MAX_ACCE = 0.2;
-    private static final double BREAK_SPEED = -0.035;
+    private static final double MIN_VELO = 75;
+    private static final double MAX_VELO = 95;
+    private static final double MIN_ACCE = 0.9;
+    private static final double MAX_ACCE = 1.2;
+    private static final double BREAK_SPEED = -0.1;
+
+    private static final int TRUCK_NUMBER = 15;
+    private static final double TMIN_VELO = 50;
+    private static final double TMAX_VELO = 55;
+    private static final double TMIN_ACCE = 0.5;
+    private static final double TMAX_ACCE = 0.6;
+    private static final double TBREAK_SPEED = -0.07;
 
     @FXML
     private Pane sim;
@@ -38,7 +45,8 @@ public class Controller {
         for (int i = 0; i < LINE_NUMBER; i++) {
             simServ.addLine(road1.getRoadId());
         }
-        createCars(CAR_NUMBER).forEach(simServ::addCar);
+        createCars(CAR_NUMBER, MIN_VELO, MAX_VELO, MIN_ACCE, MAX_ACCE, BREAK_SPEED, 8).forEach(simServ::addCar);
+        createCars(TRUCK_NUMBER, TMIN_VELO, TMAX_VELO, TMIN_ACCE, TMAX_ACCE, TBREAK_SPEED, 20).forEach(simServ::addCar);
 
         sim.getChildren().addAll(simServ.getRoadRepo().getAll());
         sim.getChildren().addAll(simServ.getCarsView());
@@ -46,11 +54,11 @@ public class Controller {
         startAnimation();
     }
 
-    private void addMouseScroll(){
+    private void addMouseScroll() {
         sim.setOnScroll(event -> {
             double zoomFactor = 1.05;
             double deltaY = event.getDeltaY();
-            if (deltaY < 0){
+            if (deltaY < 0) {
                 zoomFactor = 2.0 - zoomFactor;
             }
             sim.setScaleX(sim.getScaleX() * zoomFactor);
@@ -58,7 +66,7 @@ public class Controller {
         });
     }
 
-    private List<Car> createCars(int num) {
+    private List<Car> createCars(int num, double minVelo, double maxVelo, double minAcce, double maxAcce, double maxBreak, double carLength) {
         ThreadLocalRandom rng = ThreadLocalRandom.current();
         ArrayList<Car> result = new ArrayList<>();
         int size = simServ.getRoadRepo().getAll().size();
@@ -66,9 +74,9 @@ public class Controller {
             Road randRoad = simServ.getRoadRepo().byId(rng.nextLong(1, size + 1));
             double length = randRoad.getStartPoint2D().subtract(randRoad.getEndPoint2D()).magnitude();
             Point2D pos = randRoad.getDirection().multiply(rng.nextDouble(length)).add(randRoad.getStartPoint2D());
-            double randVelo = rng.nextDouble(MIN_VELO, MAX_VELO);
-            double randAccel = rng.nextDouble(MIN_ACCE, MAX_ACCE);
-            Car c = new Car(pos.getX(),pos.getY(),5,15, randRoad, randAccel, BREAK_SPEED, randVelo);
+            double randVelo = rng.nextDouble(minVelo, maxVelo);
+            double randAccel = rng.nextDouble(minAcce, maxAcce);
+            Car c = new Car(pos.getX(), pos.getY(), 5, carLength, randRoad, randAccel, maxBreak, randVelo);
             result.add(c);
         }
         return result;
