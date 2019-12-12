1ad114c744dff0a01e5d6a172107c92c91b0a89b
bason1515 <bason1515@gmail.com>
2019-12-12 11:56:03 +0100

    No initial vehicles on roads

--- src/main/java/Controllers/Controller.java
+++ src/main/java/Controllers/Controller.java
@@ -9,38 +9,17 @@ import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleLongProperty;
 import javafx.collections.MapChangeListener;
 import javafx.fxml.FXML;
-import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import model.car.Car;
-import model.car.CarType;
-import model.car.Limitation;
 import model.road.Road;
 import repository.CarRepository;
 import repository.CarRepositoryImpl;
 import repository.RoadRepository;
 import repository.RoadRepositoryImpl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Controller {
     private static final int LINE_NUMBER = 3;
-
-    private static final int CAR_NUMBER = 50;
-    private static final double MIN_VELO = 75;
-    private static final double MAX_VELO = 95;
-    private static final double MIN_ACCE = 0.9;
-    private static final double MAX_ACCE = 1.2;
-    private static final double BREAK_SPEED = -2;
-
-    private static final int TRUCK_NUMBER = 15;
-    private static final double TMIN_VELO = 50;
-    private static final double TMAX_VELO = 55;
-    private static final double TMIN_ACCE = 0.5;
-    private static final double TMAX_ACCE = 0.6;
-    private static final double TBREAK_SPEED = -1;
 
     @FXML
     private Pane sim;
@@ -61,7 +40,6 @@ public class Controller {
         carService = new CarService(carRepository, roadRepository);
         roadObjectService = new RoadObjectService(carRepository);
         roadsInit();
-        carsInit();
         roadObjectsInit();
         carService.getCarRepo().addListener((MapChangeListener<Long, Car>) change -> {
             if (change.wasAdded()) {
@@ -83,40 +61,11 @@ public class Controller {
         sim.getChildren().addAll(roadService.getRoadRepo().getAll());
     }
 
-    private void carsInit() {
-        createCars(CAR_NUMBER, MIN_VELO, MAX_VELO, MIN_ACCE, MAX_ACCE, BREAK_SPEED, 8).forEach(car -> {
-            car.setType(CarType.CAR);
-            carService.addCar(car);
-        });
-        createCars(TRUCK_NUMBER, TMIN_VELO, TMAX_VELO, TMIN_ACCE, TMAX_ACCE, TBREAK_SPEED, 20).forEach(car -> {
-            car.setType(CarType.TRUCK);
-            carService.addCar(car);
-        });
-        sim.getChildren().addAll(carService.getAllCarsView());
-    }
-
     private void roadObjectsInit() {
         Road road = roadService.getRoadRepo().byId(1L);
         roadObjectService.createCarCounter(road);
         roadObjectService.createVehicleSpawner(roadService.getRoadRepo().getAll().toArray(new Road[0]));
         sim.getChildren().addAll(roadObjectService.getAllViews());
-    }
-
-    private List<Car> createCars(int num, double minVelo, double maxVelo, double minAcce, double maxAcce, double maxBreak, double carLength) {
-        ThreadLocalRandom rng = ThreadLocalRandom.current();
-        ArrayList<Car> result = new ArrayList<>();
-        int size = roadService.getRoadRepo().getAll().size();
-        for (int i = 0; i < num; i++) {
-            Road randRoad = roadService.getRoadRepo().byId(rng.nextLong(1, size + 1));
-            double length = randRoad.getStartPoint2D().subtract(randRoad.getEndPoint2D()).magnitude();
-            Point2D pos = randRoad.getDirection().multiply(rng.nextDouble(length)).add(randRoad.getStartPoint2D());
-            double randVelo = rng.nextDouble(minVelo, maxVelo);
-            double randAccel = rng.nextDouble(minAcce, maxAcce);
-            Limitation limits = new Limitation(randAccel, randAccel * -1, randVelo);
-            Car car = new Car(pos, limits, 5, carLength, randRoad);
-            result.add(car);
-        }
-        return result;
     }
 
     public void startAnimation() {
